05792f5e186c57a75650669992f11629c7a47f4f
David Friday <davidsfriday13@gmail.com>
2019-06-06 01:56:44 -0500

    test

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const io = socketIO(server);
 
 // Serve static files
 
-app.use(express.static('build'));
+app.use(express.static('build/public/index.html'));
 
 /** Listen * */
 let problems = [];
