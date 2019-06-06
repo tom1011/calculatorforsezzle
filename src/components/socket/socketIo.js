97fa2b0da7b3bddbc5165e1330e23e648205f3ab
David Friday <davidsfriday13@gmail.com>
2019-06-05 21:38:28 -0500

    base url

--- src/components/socket/socketIo.js
+++ src/components/socket/socketIo.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-const ROOT_URL = ''; // defaults to base url. that should be right
+const ROOT_URL = 'https://livesimplecalculator.herokuapp.com/'; // defaults to base url. that should be right
 
 const io = require('socket.io-client/dist/socket.io');
 const socket = io.connect(ROOT_URL);
